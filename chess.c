e24e07c4434b1ba4fec155d49a46784b97ba442a
Evgeniy GA <eugeneag02@gmail.com>
2022-02-22 11:15:45 +0700

    Update chess.c

--- chess.c
+++ chess.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
-//l- ladya	h- horse	s- slon
-//q- queen	k- king		p- peshka
+//l ladya	h horse		ys slon
+//q queen	k king		p peshka
 int main()
 {
-	char A[8][8];
-	int i,j;
-        for(i = 0; i < 8; i++)
+	char A[9][9] = {0};
+	int i,j, a = 97;
+	int h = 104, k = 107, l = 108, p = 112, q = 113, s = 115;
+	A[0][0] = ' ';
+	for(i = 1; i < 9; i++)
+	{
+		A[i][0] = a;
+		a++;
+	}
+	a = 49;
+	for(j = 1; j < 9; j++)
+	{
+		A[0][j] = a;
+		a++;
+	}
+        for(i = 1; i < 9; i++)
         {
-                for(j = 0; j < 8; j++)
+                for(j = 1; j < 9; j++)
                 {
-                        if((j == 0) || (j == 7))
+			A[i][j] = ' ';
+                        if((j == 1) || (j == 8))
                         {
-                                if((i == 0) || (i == 7)) A[i][j] = 'l';
-                                if((i == 1) || (i == 6)) A[i][j] = 'h';
-                                if((i == 2) || (i == 5)) A[i][j] = 's';
-                                if(i == 3) A[i][j] = 'q';
-                                if(i == 4) A[i][j] = 'k';
+                                if((i == 1) || (i == 8)) A[i][j] = l;
+                                if((i == 2) || (i == 7)) A[i][j] = h;
+                                if((i == 3) || (i == 6)) A[i][j] = s;
+                                if(i == 4) A[i][j] = q;
+                                if(i == 5) A[i][j] = k;
                         }
-                        else    if((j == 1) || (j == 6)) A[i][j] = 'p';
-                        else
-                            	A[i][j] = ' ';
+                        if((j == 2) || (j == 7))
+			A[i][j] = p;
                 }
         }
-
-	for(j = 0; j < 8; j++)
+	for(i = 1; i < 9; i++)
 	{
-		for(i = 0; i < 8; i++)
-			printf("%c ", A[i][j]);
+		A[i][1] = A[i][1] - 32;
+		A[i][2] = A[i][2] - 32;
+	}
+
+	for(j = 0; j < 9; j++)
+	{
+		for(i = 0; i < 9; i++)
+		{
+			printf("%3c", A[i][j]);
+		}
 		printf("\n");
 	}
 	return 0;
